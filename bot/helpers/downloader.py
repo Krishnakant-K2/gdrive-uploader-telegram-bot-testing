4c1b6681f9c97811576a716495cbe1b5c8e5a9be
anon1ym <102027393+anon1ym@users.noreply.github.com>
2022-03-22 20:53:49 +0530

    Update downloader.py

--- bot/helpers/downloader.py
+++ bot/helpers/downloader.py
@@ -2,7 +2,7 @@ import os
 import wget
 import glob
 import youtube_dl
-from pySmartDL import SmartDL
+from pySmartDL import pySmartDL
 from urllib.error import HTTPError
 from youtube_dl import DownloadError
 from bot import DOWNLOAD_DIRECTORY, LOGGER
@@ -10,7 +10,7 @@ from bot import DOWNLOAD_DIRECTORY, LOGGER
 
 def download_file(url, dl_path):
   try:
-    dl = SmartDL(url, dl_path, progress_bar=False)
+    dl = pySmartDL(url, dl_path, progress_bar=False)
     LOGGER.info(f'Downloading: {url} in {dl_path}')
     dl.start()
     return True, dl.get_dest()
