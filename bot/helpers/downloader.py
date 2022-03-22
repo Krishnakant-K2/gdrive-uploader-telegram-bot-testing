8cd9d8a73712636d6f703b47328dfa7042c78ec3
anon1ym <102027393+anon1ym@users.noreply.github.com>
2022-03-22 22:30:00 +0530

    Update downloader.py

--- bot/helpers/downloader.py
+++ bot/helpers/downloader.py
@@ -23,6 +23,31 @@ def download_file(url, dl_path):
     except HTTPError:
       return False, error
 
+
+def _telegram_file(url, dl_path):
+  try:
+    dl = pySmartDL(url, dl_path, progress_bar=False)
+    LOGGER.info(f'Downloading: {url} in {dl_path}')
+    dl.start()
+    return True, dl.get_dest()
+  except HTTPError as error:
+    return False, error
+  except Exception as error:
+    try:
+      filename = wget.download(url, dl_path)
+      return True, os.path.join(f"{DOWNLOAD_DIRECTORY}/{filename}")
+    except HTTPError:
+      return False, error
+  try:
+    file_path = message.download(file_name=DOWNLOAD_DIRECTORY)
+    sent_message.edit(Messages.DOWNLOADED_SUCCESSFULLY.format(os.path.basename(file_path), humanbytes(os.path.getsize(file_path))))
+    msg = GoogleDrive(user_id).upload_file(file_path, file.mime_type)
+    sent_message.edit(msg)
+  except RPCError:
+    sent_message.edit(Messages.WENT_WRONG)
+  LOGGER.info(f'Deleteing: {file_path}')
+  os.remove(file_path)
+
 def download_fb(url, dl_path):
   try:
     filename = wget.download(url, dl_path)
