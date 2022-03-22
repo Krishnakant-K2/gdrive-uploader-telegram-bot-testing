2c88d6ad554ef2a4e2a3597b11db9c016cbfe260
anon1ym <102027393+anon1ym@users.noreply.github.com>
2022-03-22 19:33:19 +0530

    Update downloader.py

--- bot/helpers/downloader.py
+++ bot/helpers/downloader.py
@@ -10,18 +10,18 @@ from bot import DOWNLOAD_DIRECTORY, LOGGER
 
 def download_file(url, dl_path):
   try:
-    dl = SmartDL(url, dl_path, progress_bar)
+    dl = SmartDL(url, dl_path, progress_bar=False)
     LOGGER.info(f'Downloading: {url} in {dl_path}')
     dl.start()
-    return , dl.get_dest()
+    return False, dl.get_dest()
   except HTTPError as error:
-    return , error
+    return False, error
   except Exception as error:
     try:
       filename = wget.download(url, dl_path)
-      return , os.path.join(f"{DOWNLOAD_DIRECTORY}/{filename}")
+      return False, os.path.join(f"{DOWNLOAD_DIRECTORY}/{filename}")
     except HTTPError:
-      return , error
+      return False, error
 
 
 def utube_dl(link):
@@ -40,5 +40,5 @@ def utube_dl(link):
     for path in glob.glob(os.path.join(DOWNLOAD_DIRECTORY, '*')):
       if path.endswith(('.avi', '.mov', '.flv', '.wmv', '.3gp','.mpeg', '.webm', '.mp4', '.mkv')) and \
           path.startswith(ytdl.prepare_filename(meta)):
-        return , path
-    return , 'Something went wrong! No video file exists on server.'
+        return False, path
+    return False, 'Something went wrong! No video file exists on server.'
